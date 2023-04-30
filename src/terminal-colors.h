fb3655e72b07358d3f178d18cae310fed44b09b8
Eduin Garcia Cordero <eduinlight@gmail.com>
2023-05-01 01:18:03 +0200

    add macros

--- src/terminal-colors.h
+++ src/terminal-colors.h
@@ -1,3 +1,6 @@
+#ifndef C_TERMINAL_COLORS_HEADER
+#define C_TERMINAL_COLORS_HEADER
+
 #define TERMINAL_COLOR_BLACK "\033[0;30m"
 #define TERMINAL_COLOR_RED "\033[0;31m"
 #define TERMINAL_COLOR_GREEN "\033[0;32m"
@@ -6,4 +9,16 @@
 #define TERMINAL_COLOR_PURPLE "\033[0;35m"
 #define TERMINAL_COLOR_CYAN "\033[0;36m"
 #define TERMINAL_COLOR_WHITE "\033[0;37m"
-#define TERMINAL_COLOR_RESET "\033[0m"
+#define TERMINAL_COLOR_DEFAULT "\033[0m"
+
+#define set_tc_black() printf("%s", TERMINAL_COLOR_BLACK);
+#define set_tc_red() printf("%s", TERMINAL_COLOR_RED);
+#define set_tc_green() printf("%s", TERMINAL_COLOR_GREEN);
+#define set_tc_yellow() printf("%s", TERMINAL_COLOR_YELLOW);
+#define set_tc_blue() printf("%s", TERMINAL_COLOR_BLUE);
+#define set_tc_purple() printf("%s", TERMINAL_COLOR_PURPLE);
+#define set_tc_cyan() printf("%s", TERMINAL_COLOR_CYAN);
+#define set_tc_white() printf("%s", TERMINAL_COLOR_WHITE);
+#define set_tc_default() printf("%s", TERMINAL_COLOR_RESET);
+
+#endif // !C_TERMINAL_COLORS_HEADER
